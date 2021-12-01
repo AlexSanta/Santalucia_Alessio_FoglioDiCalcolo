074e220e192518ad6a35ddf737ad31aefa06ff40
Alessio <santacips99@gmail.com>
2021-12-01 14:23:41 +0100

    Test MVC alternativo

---
+++ test/MVCAlternativeTest.cpp
@@ -0,0 +1,37 @@
+#include "gtest/gtest.h"
+
+#include "../View.cpp"
+
+TEST(MVC, FalseChange){
+    Model m;
+    Controller c(&m);
+    View v(&m,&c);
+    v.getUi()->tW->item(1,0)->setText("12");
+    v.getUi()->tW->item(1,1)->setText("3.5");
+    v.getUi()->tW->item(1,2)->setText("7");
+    v.getUi()->tW->item(1,3)->setText("-4");
+
+    v.getUi()->tW->item(1,0)->setText("a");
+    ASSERT_EQ("12", v.getUi()->tW->item(1,0)->text());
+    ASSERT_EQ("18.5", v.getUi()->tW->item(1,4)->text());
+    ASSERT_EQ("4.625", v.getUi()->tW->item(1,5)->text());
+    ASSERT_EQ("-4", v.getUi()->tW->item(1,6)->text());
+    ASSERT_EQ("12", v.getUi()->tW->item(1,7)->text());
+}
+
+TEST(MVC, Change){
+    Model m;
+    Controller c(&m);
+    View v(&m,&c);
+    v.getUi()->tW->item(1,0)->setText("12");
+    v.getUi()->tW->item(1,1)->setText("3.5");
+    v.getUi()->tW->item(1,2)->setText("7");
+    v.getUi()->tW->item(1,3)->setText("-4");
+
+    v.getUi()->tW->item(1,1)->setText("23");
+    ASSERT_EQ(1, m.getIndex());
+    ASSERT_EQ("38", v.getUi()->tW->item(1,4)->text());
+    ASSERT_EQ("9.5", v.getUi()->tW->item(1,5)->text());
+    ASSERT_EQ("-4", v.getUi()->tW->item(1,6)->text());
+    ASSERT_EQ("23", v.getUi()->tW->item(1,7)->text());
+}
